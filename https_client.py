f77a4a1d0fe9bce00ae7e93f55310f56ac2eb831
Zamoo117 <155308482+Zamoo117@users.noreply.github.com>
2024-07-05 15:30:48 +0000

    Erneut kleine Korrektur

--- https_client.py
+++ https_client.py
@@ -3,7 +3,7 @@ import requests
 
 # Funktion, um eine HTTPS-Anfrage zu erstellen
 def make_request(url='https://localhost:8443', cert='path/to/cert.pem'):
-    response = requests.get(url, verify=verify)  # Sendet eine GET-Anfrage an die angegebene URL, wobei die SSL-Zertifikatsprüfung deaktiviert ist
+    response = requests.get(url, verify=cert)  # Sendet eine GET-Anfrage an die angegebene URL, wobei die SSL-Zertifikatsprüfung deaktiviert ist
     print(f'Response: {response.text}')  # Gibt den Antworttext der Anfrage aus
 
 # Überprüft, ob das Skript direkt ausgeführt wird
